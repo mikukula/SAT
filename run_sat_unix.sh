b1660473166f3561cb2f3e878ddcf5db4daafed2
mikukula <mikukula123@gmail.com>
2025-01-13 02:05:58 +0000

    add installation script and improve run script for better directory handling

--- run_sat_unix.sh
+++ run_sat_unix.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-cd ~/Documents/SAT
+# Get the directory where the script is located
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd $SCRIPT_DIR
 source venv/bin/activate
 python3 src/main.py
+deactivate
