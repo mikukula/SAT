50db1be669fdb593a6f203555562e6ec4cf3b81d
mikukula <mikukula@users.noreply.github.com>
2025-01-13 02:11:10 +0000

    Revert "add installation script and improve run script for better directory handling"
    This reverts commit b1660473166f3561cb2f3e878ddcf5db4daafed2.

--- run_sat_unix.sh
+++ run_sat_unix.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Get the directory where the script is located
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-cd $SCRIPT_DIR
+cd ~/Documents/SAT
 source venv/bin/activate
 python3 src/main.py
-deactivate
